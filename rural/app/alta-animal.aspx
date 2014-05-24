d01582911d82647dcc4847499a02a7f8fcb68ada
mery <metanoia023@gmail.com>
2014-05-24 15:23:58 -0300

    create 3 proyects app domain and mapper, animal db and establecimiento db, first web form Alta Animal, next code behind. clases: animal. missing: establecimiento.cs.

---
+++ rural/app/alta-animal.aspx
@@ -0,0 +1,102 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="alta-animal.aspx.cs" Inherits="app.alta_animal" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+    <link href="style.css" rel="stylesheet" />
+    <title></title>
+    <style type="text/css">
+        .auto-style1 {
+            width: 152px;
+        }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+    
+    <h1>Alta de animal</h1>
+        <table>
+            <tr>
+                <td class="auto-style1">Identificador SNIG</td>
+                <td>
+                    <asp:TextBox ID="tbxIdOficial" runat="server" OnTextChanged="TextBox1_TextChanged"></asp:TextBox>
+                </td>
+                
+            </tr>
+            <tr>
+                <td class="auto-style1">Identificador local</td>
+                <td>
+                    <asp:TextBox ID="tbxIdLocal" runat="server"></asp:TextBox>
+                </td>
+               
+            </tr>
+            <tr>
+                <td class="auto-style1">Anio de nacimiento</td>
+                <td>
+                    <asp:TextBox ID="tbxAnioNacimiento" runat="server"></asp:TextBox>
+                </td>
+               
+            </tr>
+            <tr>
+                <td class="auto-style1">Estacion</td>
+                <td>
+                    <asp:DropDownList ID="ddlEstacion" runat="server">
+                        <asp:ListItem>Verano</asp:ListItem>
+                        <asp:ListItem>Otonio</asp:ListItem>
+                        <asp:ListItem>Invierno</asp:ListItem>
+                        <asp:ListItem>Primavera</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+               
+            </tr>
+            <tr>
+                <td class="auto-style1">Sexo</td>
+                <td>
+                    <asp:DropDownList ID="ddlSexo" runat="server">
+                        <asp:ListItem>Macho</asp:ListItem>
+                        <asp:ListItem>Hembra</asp:ListItem>
+                    </asp:DropDownList>
+                </td>
+               
+            </tr>
+             <tr>
+                <td class="auto-style1">Raza</td>
+                <td>
+                    <asp:TextBox ID="tbxRaza" runat="server"></asp:TextBox>
+                 </td>
+               
+            </tr>
+             <tr>
+                <td class="auto-style1">Cruza</td>
+                <td>
+                    <asp:TextBox ID="tbxCruza" runat="server"></asp:TextBox>
+                 </td>
+               
+            </tr>
+             <tr>
+                <td class="auto-style1">Dicose del propietario</td>
+                <td>
+                    <asp:TextBox ID="tbxDicosePropietario" runat="server"></asp:TextBox>
+                 </td>
+               
+            </tr>
+             <tr>
+                <td class="auto-style1">&nbsp;</td>
+                <td>
+                    <asp:Button ID="btnAltaAnimal" runat="server" OnClick="Button1_Click" Text="Aceptar" />
+                    <asp:Button ID="btnCancelarAlta" runat="server" Text="Cancelar" />
+                 </td>
+               
+            </tr>
+
+         
+        </table>
+    
+    
+    </div>
+    </form>
+</body>
+</html>
---
+++ rural/app/alta-animal.aspx.designer.cs
@@ -0,0 +1,114 @@
+﻿//------------------------------------------------------------------------------
+// <generado automáticamente>
+//     Este código fue generado por una herramienta.
+//
+//     Los cambios en este archivo podrían causar un comportamiento incorrecto y se perderán si
+//     se vuelve a generar el código. 
+// </generado automáticamente>
+//------------------------------------------------------------------------------
+
+namespace app {
+    
+    
+    public partial class alta_animal {
+        
+        /// <summary>
+        /// Control form1.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlForm form1;
+        
+        /// <summary>
+        /// Control tbxIdOficial.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbxIdOficial;
+        
+        /// <summary>
+        /// Control tbxIdLocal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbxIdLocal;
+        
+        /// <summary>
+        /// Control tbxAnioNacimiento.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbxAnioNacimiento;
+        
+        /// <summary>
+        /// Control ddlEstacion.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlEstacion;
+        
+        /// <summary>
+        /// Control ddlSexo.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.DropDownList ddlSexo;
+        
+        /// <summary>
+        /// Control tbxRaza.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbxRaza;
+        
+        /// <summary>
+        /// Control tbxCruza.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbxCruza;
+        
+        /// <summary>
+        /// Control tbxDicosePropietario.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.TextBox tbxDicosePropietario;
+        
+        /// <summary>
+        /// Control btnAltaAnimal.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnAltaAnimal;
+        
+        /// <summary>
+        /// Control btnCancelarAlta.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.Button btnCancelarAlta;
+    }
+}
